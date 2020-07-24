2b2758fc6fc8199554504981cd4bdca51afe1a36
Abhinav Barve <abhinav.barve2001@gmail.com>
2020-07-24 11:02:24 +0530

    minor style changes

--- src/components/Images/Images.jsx
+++ src/components/Images/Images.jsx
@@ -9,7 +9,7 @@ function Images(){
 
     const [images, setImages] = useState([]);
     const API_KEY = "17530347-2fa43ec2c758fad8cc3310156";
-    const url = 'https://pixabay.com/api/?key=' + API_KEY + '&q='+ searchTerm + '&image_type=photo&per_page=200'
+    const url = 'https://pixabay.com/api/?key=' + API_KEY + '&q='+ searchTerm + '&image_type=photo&per_page=30'
 
     function submit(input){
         setSearchTerm(input)
