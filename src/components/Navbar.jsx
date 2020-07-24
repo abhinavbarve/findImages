5d313a61fcf141a24c3f84f694eff3c89a120507
Abhinav Barve <abhinav.barve2001@gmail.com>
2020-07-24 10:10:45 +0530

    navbar change mr-auto

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ function Navbar(){
   </button>
 
   <div className="collapse navbar-collapse" id="navigation-bar">
-    <ul className="navbar-nav mr-auto">
+    <ul className="navbar-nav ml-auto">
       <li className="nav-item active">
         <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
       </li>
